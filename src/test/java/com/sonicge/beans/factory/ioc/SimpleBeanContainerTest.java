e23b16843427586c3311a6f961595c1018a5a2ca
etge <etge@iflytek.com>
2025-11-21 10:13:18 +0800

    test(mini-spring):测试‘新增Bean的实例化策略InstantiationStrategy接口’

--- src/test/java/com/sonicge/beans/factory/ioc/SimpleBeanContainerTest.java
+++ src/test/java/com/sonicge/beans/factory/ioc/SimpleBeanContainerTest.java
@@ -17,16 +17,30 @@ public class SimpleBeanContainerTest {
 
     @Test
     public void testBeanFactory_v2() {
+//        String beanName = "helloService";
+//        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+//        //创建BeanDefinition对象
+//        BeanDefinition beanDefinition = new BeanDefinition(HelloService.class);
+//        //将BeanDefinition对象注册到beanDefinitionMap中（DefaultListableBeanFactory类的属性）
+//        beanFactory.registerBeanDefinition(beanName, beanDefinition);
+//        //注册好了之后，就可以调用getBean方法（AbstractBeanFactory父类中的，部分的实现还要依赖于AbstractAutowireCapableBeanFactory）
+//        HelloService helloService = (HelloService) beanFactory.getBean(beanName);
+//        //调用方法。。。
+//        String returnValue = helloService.sayHello();
+//        System.out.println(returnValue);
+    }
+
+    /**
+     * Bean实例化策略InstantiationStrategy
+     */
+    @Test
+    public void testBeanFactory_v3(){
         String beanName = "helloService";
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        //创建BeanDefinition对象
         BeanDefinition beanDefinition = new BeanDefinition(HelloService.class);
-        //将BeanDefinition对象注册到beanDefinitionMap中（DefaultListableBeanFactory类的属性）
-        beanFactory.registerBeanDefinition(beanName, beanDefinition);
-        //注册好了之后，就可以调用getBean方法（AbstractBeanFactory父类中的，部分的实现还要依赖于AbstractAutowireCapableBeanFactory）
+        beanFactory.registerBeanDefinition(beanName,beanDefinition);
+
         HelloService helloService = (HelloService) beanFactory.getBean(beanName);
-        //调用方法。。。
-        String returnValue = helloService.sayHello();
-        System.out.println(returnValue);
+        helloService.sayHello();
     }
 }
