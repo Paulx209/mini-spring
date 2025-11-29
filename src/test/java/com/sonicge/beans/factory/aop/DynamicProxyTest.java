2d63f3f086bec057f33d57afd0e642a312e32cd1
etge <etge@iflytek.com>
2025-11-29 16:01:26 +0800

    feat(cglib-dynamic-proxy )：修改属性权限修饰符

--- src/test/java/com/sonicge/beans/factory/aop/DynamicProxyTest.java
+++ src/test/java/com/sonicge/beans/factory/aop/DynamicProxyTest.java
@@ -1,8 +1,10 @@
 package com.sonicge.beans.factory.aop;
 
 import com.sonicge.aop.AdvicedSupport;
+import com.sonicge.aop.MethodMatcher;
 import com.sonicge.aop.TargetSource;
 import com.sonicge.aop.aspectj.AspectJExpressionPointcut;
+import com.sonicge.aop.framework.CglibAopProxy;
 import com.sonicge.aop.framework.JdkDynamicAopProxy;
 import com.sonicge.beans.factory.common.WorldServiceInterceptor;
 import com.sonicge.beans.factory.service.WorldService;
@@ -10,8 +12,11 @@ import com.sonicge.beans.factory.service.WorldServiceImpl;
 import org.junit.Test;
 
 public class DynamicProxyTest {
+
+    private AdvicedSupport advicedSupport;
+
     @Test
-    public void test(){
+    public void test() {
         //创建worldService类，构建targetSource类
         WorldService worldService = new WorldServiceImpl();
         TargetSource targetSource = new TargetSource(worldService);
@@ -39,5 +44,27 @@ public class DynamicProxyTest {
         //以前的增强类代码都直接写到InvocationHandler，也就是JdkDynamicAopProxy里面。现在封装到MethodInterceptor里面，不直接暴露出来
 
         //AspectJExpressionPointcut -> methodMatcher类
+    }
+
+    @Test
+    public void testCglibDynamicProxy() {
+        //创建worldService类，构建targetSource类
+        WorldService worldService = new WorldServiceImpl();
+        TargetSource targetSource = new TargetSource(worldService);
+
+        //构建MethodInterceptor类
+        WorldServiceInterceptor worldServiceInterceptor = new WorldServiceInterceptor();
+
+        //构建MethodMatcher类  -> AspectJExpressionPointcut
+        MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* com.sonicge.beans.factory.service..*(..))").getMethodMatcher();
+
+        //创建AdvicedSupport类
+        AdvicedSupport advicedSupport = new AdvicedSupport();
+        advicedSupport.setTargetSource(targetSource);
+        advicedSupport.setMethodInterceptor(worldServiceInterceptor);
+        advicedSupport.setMethodMatcher(methodMatcher);
+
+        WorldServiceImpl proxy = (WorldServiceImpl) new CglibAopProxy(advicedSupport).getProxy();
+        proxy.explode();
     }
 }
