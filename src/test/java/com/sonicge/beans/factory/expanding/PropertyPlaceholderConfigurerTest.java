3d31ce3509ed64e791bdd111181e701144fa007a
etge <etge@iflytek.com>
2025-12-01 18:10:38 +0800

    test(property-placeholder-configurer)：PropertyPlaceholderConfigurer 测试

---
+++ src/test/java/com/sonicge/beans/factory/expanding/PropertyPlaceholderConfigurerTest.java
@@ -0,0 +1,17 @@
+package com.sonicge.beans.factory.expanding;
+
+import com.sonicge.beans.BeansException;
+import com.sonicge.beans.context.ApplicationContext;
+import com.sonicge.beans.factory.aop.bean.Car;
+import com.sonicge.beans.support.ClassPathXmlApplicationContext;
+import org.junit.Test;
+
+public class PropertyPlaceholderConfigurerTest {
+    @Test
+    public void test() throws BeansException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:property-placeholder-configurer.xml");
+
+        Car car = (Car) context.getBean("car");
+        System.out.println(car);
+    }
+}
